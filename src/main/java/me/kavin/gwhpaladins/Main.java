f143378211533c182158d46f803d0ad491e8e397
FireMasterK <kavinsundar@gmail.com>
2018-05-15 10:45:45 +0530

    Signed-off-by: FireMasterK <kavinsundar@gmail.com>

--- src/main/java/me/kavin/gwhpaladins/Main.java
+++ src/main/java/me/kavin/gwhpaladins/Main.java
@@ -1,7 +1,5 @@
 package me.kavin.gwhpaladins;
 
-import java.util.ArrayList;
-
 import javax.security.auth.login.LoginException;
 
 import me.kavin.gwhpaladins.command.CommandManager;
@@ -10,14 +8,12 @@ import me.kavin.gwhpaladins.utils.Timer;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
-import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.exceptions.RateLimitedException;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 public class Main extends ListenerAdapter{
 	public static JDA api;
 	public static Timer uptime = new Timer();
-	public static ArrayList<TextChannel> channels;
 
 	public static void main(String[] args) {
 		try {
--- src/main/java/me/kavin/gwhpaladins/listener/DiscordListener.java
+++ src/main/java/me/kavin/gwhpaladins/listener/DiscordListener.java
@@ -1,27 +1,24 @@
 package me.kavin.gwhpaladins.listener;
 
 import java.awt.Color;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-
 import me.kavin.gwhpaladins.Main;
 import me.kavin.gwhpaladins.command.Command;
 import me.kavin.gwhpaladins.command.CommandManager;
-
+import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.entities.ChannelType;
 import net.dv8tion.jda.core.entities.Game;
-import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.entities.Game.GameType;
 import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Role;
 import net.dv8tion.jda.core.events.ReadyEvent;
+import net.dv8tion.jda.core.events.guild.GuildJoinEvent;
 import net.dv8tion.jda.core.events.guild.update.GuildUpdateOwnerEvent;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.events.message.priv.PrivateMessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
-public class DiscordListener extends ListenerAdapter{
+public class DiscordListener extends ListenerAdapter {
 	
 	HashMap<String, String> lastMsg = new HashMap<String, String>();
 	
@@ -31,21 +28,18 @@ public class DiscordListener extends ListenerAdapter{
 	@Override
 	public void onReady(ReadyEvent event) {
 		Main.api.getPresence().setGame(Game.of(GameType.DEFAULT, "Meminq | .help", "Hax.kill"));
-		Main.channels = new ArrayList<TextChannel>(Main.api.getTextChannels());
-		
-		List<Guild> guilds = Main.api.getGuilds();
 		
 		new Thread(new Runnable() {
 			@Override
 			public void run() {
 				while(true){
-					for(Guild g : guilds){
+					for(Guild g : Main.api.getGuilds()){
 						for(Role role : g.getRoles()){
-							if(role.getName().equalsIgnoreCase("rainbow"))
-							role.getManager().setColor(getRainbowColor(60000)).queue();
-							try {
-								Thread.sleep(200);
-							} catch (InterruptedException e) {
+							if(role.getName().equalsIgnoreCase("rainbow")) {
+								role.getManager().setColor(getRainbowColor(120000)).queue();
+								try {
+									Thread.sleep(200);
+								} catch (InterruptedException e) { }
 							}
 						}
 					}
@@ -54,27 +48,21 @@ public class DiscordListener extends ListenerAdapter{
 			}
 		}).start();
 	}
+	
+	@Override
+	public void onGuildJoin(GuildJoinEvent event) {
+		if (!event.getGuild().getMember(Main.api.getSelfUser()).hasPermission(Permission.ADMINISTRATOR)) {
+			event.getGuild().getDefaultChannel().sendMessage("Please ask a server admistrator to invite me!").queue();
+			event.getGuild().leave().queue();
+		}
+	}
+	
 	@Override
 	public void onGuildUpdateOwner(GuildUpdateOwnerEvent event) {
-		event.getGuild().getTextChannels().forEach( tc -> {
-			
-		});
+		event.getGuild().getOwner().getUser().openPrivateChannel().complete().sendMessage("Congrats on becoming the new owner of " + event.getGuild().getName()).queue();
 	}
 	@Override
 	public void onMessageReceived(MessageReceivedEvent event) {
-	if(event.getAuthor().isBot() || event.getMessage().getRawContent().startsWith(".") || event.getMessage().getRawContent().startsWith("t!") || event.getMessage().getRawContent().startsWith("`")){
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				try {
-					Thread.sleep(60000);
-					event.getMessage().delete().queue();
-				} catch (InterruptedException e) {
-				}
-				event.getMessage().delete().queue();
-			}
-		}).start();
-	} else {
 		if(!event.getAuthor().isBot() && lastMsg.containsKey(event.getAuthor().getName())) {
 			if(lastMsg.get(event.getAuthor().getName()).startsWith(event.getMessage().getRawContent()) && event.getMessage().getAttachments().isEmpty()) {
 				event.getChannel().sendMessage("You may not spam!").queue();
@@ -82,7 +70,6 @@ public class DiscordListener extends ListenerAdapter{
 			}
 		}
 		lastMsg.put(event.getAuthor().getName(), event.getMessage().getRawContent());
-	}
 	if (event.isFromType(ChannelType.PRIVATE) || event.getAuthor() == Main.api.getSelfUser()){
 		return;
 	}
