026e516d9511d4c079ffbc4c77f7032ef5fabd0c
FireMasterK <kavinsundar@gmail.com>
2018-07-02 16:54:20 +0530

    Format code

--- src/main/java/me/kavin/mememachine/command/commands/Meme.java
+++ src/main/java/me/kavin/mememachine/command/commands/Meme.java
@@ -18,7 +18,7 @@ import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 public class Meme extends Command {
 
 	WebClient wc = new WebClient();
-	
+
 	String lastData = null;
 	long lastUpdate = 0;
 
@@ -34,19 +34,20 @@ public class Meme extends Command {
 	private MessageEmbed getMeme() {
 		try {
 			EmbedBuilder meb = new EmbedBuilder();
-			if(System.currentTimeMillis() - lastUpdate > 300000)
-				lastData = wc.getPage("https://www.reddit.com/r/memes/top/.json?sort=top&t=day&limit=250").getWebResponse().getContentAsString();
+			if (System.currentTimeMillis() - lastUpdate > 300000)
+				lastData = wc.getPage("https://www.reddit.com/r/memes/top/.json?sort=top&t=day&limit=250")
+						.getWebResponse().getContentAsString();
 			JSONObject root = new JSONObject(lastData);
 			JSONArray posts = root.getJSONObject("data").getJSONArray("children");
-				JSONObject post = posts.getJSONObject(ThreadLocalRandom.current().nextInt(posts.length())).getJSONObject("data");
-				meb.setTitle(post.getString("title"));
-				meb.setColor(ColorUtils.getRainbowColor(2000));
-				meb.setImage(Constants.GOOGLE_PROXY_IMAGE + URLEncoder.encode(post.getString("url"), "UTF-8"));
-				String author = post.getString("author");
-				meb.setAuthor(author, "https://www.reddit.com/user/" + author);
-				meb.setFooter(
-						"\uD83D\uDC4D" + post.getInt("ups") + " | " + "\uD83D\uDCAC" + post.getInt("num_comments"),
-						Main.api.getSelfUser().getAvatarUrl());
+			JSONObject post = posts.getJSONObject(ThreadLocalRandom.current().nextInt(posts.length()))
+					.getJSONObject("data");
+			meb.setTitle(post.getString("title"));
+			meb.setColor(ColorUtils.getRainbowColor(2000));
+			meb.setImage(Constants.GOOGLE_PROXY_IMAGE + URLEncoder.encode(post.getString("url"), "UTF-8"));
+			String author = post.getString("author");
+			meb.setAuthor(author, "https://www.reddit.com/user/" + author);
+			meb.setFooter("\uD83D\uDC4D" + post.getInt("ups") + " | " + "\uD83D\uDCAC" + post.getInt("num_comments"),
+					Main.api.getSelfUser().getAvatarUrl());
 			return meb.build();
 		} catch (Exception e) {
 			return null;
