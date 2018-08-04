cbde9c225ca8d6deed7824f53fe5f8388dbabc96
FireMasterK <kavinsundar@gmail.com>
2018-08-04 13:33:17 +0530

    fix aww command

--- src/main/java/me/kavin/mememachine/command/commands/Aww.java
+++ src/main/java/me/kavin/mememachine/command/commands/Aww.java
@@ -1,16 +1,16 @@
 package me.kavin.mememachine.command.commands;
 
-import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
-import org.json.JSONTokener;
-
 import com.gargoylesoftware.htmlunit.WebClient;
 
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import me.kavin.mememachine.Main;
 import me.kavin.mememachine.command.Command;
 import me.kavin.mememachine.utils.ColorUtils;
+import me.kavin.mememachine.utils.reddit.ImagePostData;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.entities.MessageEmbed;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
@@ -18,6 +18,9 @@ import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 public class Aww extends Command {
 
 	WebClient wc = new WebClient();
+
+	ObjectArrayList<ImagePostData> lastData = new ObjectArrayList<>();
+	long lastUpdate = 0;
 
 	public Aww() {
 		super(">aww", "`Shows a cute image`");
@@ -31,34 +34,41 @@ public class Aww extends Command {
 	private MessageEmbed getPost() {
 		try {
 			EmbedBuilder meb = new EmbedBuilder();
-			String data = wc.getPage("https://gateway.reddit.com/desktopapi/v1/subreddits/aww?sort=hot")
-					.getWebResponse().getContentAsString();
-			int tries = 0;
-			JSONTokener tokener = new JSONTokener(data);
-			JSONObject root = new JSONObject(tokener);
-			boolean found = false;
-			JSONObject posts = root.getJSONObject("posts");
-			String[] keys = Arrays.copyOf(posts.keySet().toArray(), posts.keySet().size(), String[].class);
-			while (!found && tries <= 5) {
-				JSONObject post = posts.getJSONObject(keys[ThreadLocalRandom.current().nextInt(keys.length)]);
-				if (post.getBoolean("isLocked") || post.isNull("media")
-						|| !post.getJSONObject("media").has("content")) {
-					tries++;
-					continue;
+			if (System.currentTimeMillis() - lastUpdate > 300000) {
+
+				JSONObject root = new JSONObject(
+						wc.getPage("https://www.reddit.com/r/aww/top/.json?sort=top&t=day&limit=250").getWebResponse()
+								.getContentAsString());
+
+				JSONArray posts = root.getJSONObject("data").getJSONArray("children");
+
+				lastData.clear();
+
+				for (int i = 0; i < posts.length(); i++) {
+					JSONObject post = posts.getJSONObject(i).getJSONObject("data");
+					String img_url = post.getString("url");
+					if (img_url.contains("i.redd.it"))
+						lastData.add(new ImagePostData(post.getString("title"), post.getString("author"), img_url,
+								post.getInt("num_comments"), post.getInt("ups")));
 				}
-				found = true;
-				meb.setTitle(post.getString("title"));
-				meb.setColor(ColorUtils.getRainbowColor(2000));
-				meb.setImage(post.getJSONObject("media").getString("content"));
-				String author = post.getString("author");
-				meb.setAuthor(author, "https://www.reddit.com/user/" + author);
-				meb.setFooter(
-						"\uD83D\uDC4D" + post.getInt("score") + " | " + "\uD83D\uDCAC" + post.getInt("numComments"),
-						Main.api.getSelfUser().getAvatarUrl());
+				lastUpdate = System.currentTimeMillis();
 			}
+
+			ImagePostData imagePostData = lastData.get(ThreadLocalRandom.current().nextInt(lastData.size()));
+
+			String author = imagePostData.author;
+
+			meb.setTitle(imagePostData.title);
+			meb.setColor(ColorUtils.getRainbowColor(2000));
+			meb.setImage(imagePostData.img_url);
+			meb.setAuthor(author, "https://www.reddit.com/user/" + author);
+			meb.setFooter(
+					"\uD83D\uDC4D" + imagePostData.num_upvotes + " | " + "\uD83D\uDCAC" + imagePostData.num_comments,
+					Main.api.getSelfUser().getAvatarUrl());
+
 			return meb.build();
-		} catch (Throwable t) {
+		} catch (Exception e) {
+			return null;
 		}
-		return null;
 	}
 }
