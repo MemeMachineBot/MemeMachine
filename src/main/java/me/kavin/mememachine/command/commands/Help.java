47b9366292ad8a7c8ceaf510970c2fdf7c61bc7c
FireMasterK <kavinsundar@gmail.com>
2018-08-07 21:34:11 +0530

    revert

--- src/main/java/me/kavin/mememachine/command/commands/Help.java
+++ src/main/java/me/kavin/mememachine/command/commands/Help.java
@@ -4,6 +4,7 @@ import me.kavin.mememachine.command.Command;
 import me.kavin.mememachine.command.CommandManager;
 import me.kavin.mememachine.utils.ColorUtils;
 import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.PrivateChannel;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
 public class Help extends Command {
@@ -16,17 +17,18 @@ public class Help extends Command {
 		EmbedBuilder meb = new EmbedBuilder();
 		meb.setColor(ColorUtils.getRainbowColor(2000));
 		meb.setTitle("Meme Machine's Commands:");
+		PrivateChannel pc = event.getAuthor().openPrivateChannel().complete();
 		for (Command cmd : CommandManager.commands) {
 			if (meb.getFields().size() >= 25) {
-				event.getChannel().sendMessage(meb.build()).queue();
+				pc.sendMessage(meb.build()).queue();
 				meb.clearFields();
-				meb.addField(cmd.getPrefix(), cmd.getHelp() + '\n', true);
+				meb.addField(cmd.getPrefix(), cmd.getHelp() + '\n', false);
 			} else {
-				meb.addField(cmd.getPrefix(), cmd.getHelp() + '\n', true);
+				meb.addField(cmd.getPrefix(), cmd.getHelp() + '\n', false);
 			}
 
 		}
 		if (meb.getFields().size() > 0)
-			event.getChannel().sendMessage(meb.build()).queue();
+			pc.sendMessage(meb.build()).queue();
 	}
 }
