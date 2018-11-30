2560a20585bf1a0287a3019ce3b8f509f690edb5
FireMasterK <kavin@rape.lol>
2018-11-30 13:25:59 +0530

    Cleanup Main and Timer

--- src/main/java/me/kavin/mememachine/Main.java
+++ src/main/java/me/kavin/mememachine/Main.java
@@ -1,5 +1,7 @@
 package me.kavin.mememachine;
 
+import com.mashape.unirest.http.Unirest;
+
 import me.kavin.mememachine.command.CommandManager;
 import me.kavin.mememachine.consts.Constants;
 import me.kavin.mememachine.listener.DiscordListener;
@@ -10,16 +12,17 @@ import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 public class Main extends ListenerAdapter {
+
 	public static JDA api;
 	public static Timer uptime = new Timer();
 
 	public static void main(String[] args) {
 		try {
 			JDABuilder builder = new JDABuilder(AccountType.BOT).setToken(Constants.BOT_TOKEN);
-			api = builder.buildAsync();
+			api = builder.build();
 			api.setAutoReconnect(true);
+			Unirest.setDefaultHeader("User-Agent", "Meme Machine: uptime-" + uptime.getTime());
 			DiscordListener.init();
-			uptime.reset();
 			new CommandManager();
 		} catch (Exception e) {
 			e.printStackTrace();
