de90c026add8af380117d73589c1f3f857e8ba75
FireMasterk <20838718+FireMasterK@users.noreply.github.com>
2020-06-22 02:48:00 +0530

    Add changes for low ram.

--- src/main/java/me/kavin/mememachine/Main.java
+++ src/main/java/me/kavin/mememachine/Main.java
@@ -5,8 +5,12 @@ import me.kavin.mememachine.consts.Constants;
 import me.kavin.mememachine.listener.DiscordListener;
 import me.kavin.mememachine.utils.Timer;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
 import net.dv8tion.jda.api.sharding.ShardManager;
+import net.dv8tion.jda.api.utils.ChunkingFilter;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
 public class Main extends ListenerAdapter {
 
@@ -16,6 +20,11 @@ public class Main extends ListenerAdapter {
 	public static void main(String[] args) {
 		try {
 			DefaultShardManagerBuilder builder = new DefaultShardManagerBuilder(Constants.BOT_TOKEN);
+			builder.disableCache(CacheFlag.ACTIVITY);
+			builder.setMemberCachePolicy(MemberCachePolicy.VOICE.or(MemberCachePolicy.OWNER));
+			builder.setChunkingFilter(ChunkingFilter.NONE);
+			builder.disableIntents(GatewayIntent.GUILD_PRESENCES, GatewayIntent.GUILD_MESSAGE_TYPING);
+			builder.setLargeThreshold(50);
 			builder.setAutoReconnect(true);
 			builder.addEventListeners(new DiscordListener());
 			api = builder.build();
