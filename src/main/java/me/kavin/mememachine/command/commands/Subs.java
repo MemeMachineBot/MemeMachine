3cbcae5927ed8dfe1f1a783efdbaf698060a2eab
FireMasterk <20838718+FireMasterK@users.noreply.github.com>
2020-07-02 22:49:08 +0530

    Use Invidious's api Instead of google's API

--- src/main/java/me/kavin/mememachine/command/commands/Subs.java
+++ src/main/java/me/kavin/mememachine/command/commands/Subs.java
@@ -3,12 +3,10 @@ package me.kavin.mememachine.command.commands;
 import java.net.URLEncoder;
 import java.text.DecimalFormat;
 
-import kong.unirest.json.JSONObject;
-
 import kong.unirest.Unirest;
-
+import kong.unirest.json.JSONArray;
+import kong.unirest.json.JSONObject;
 import me.kavin.mememachine.command.Command;
-import me.kavin.mememachine.consts.Constants;
 import me.kavin.mememachine.utils.ColorUtils;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
@@ -22,59 +20,47 @@ public class Subs extends Command {
 	}
 
 	@Override
-	public void onCommand(String message, MessageReceivedEvent event) {
-		try {
-			String q = null;
+	public void onCommand(String message, MessageReceivedEvent event) throws Exception {
+		String q = null;
 
-			if (message.length() > getPrefix().length()) {
-				q = "";
-				for (int i = getPrefix().length() + 1; i < message.length(); i++)
-					q += message.charAt(i);
-			}
-			
-			if (q == null) {
-				EmbedBuilder meb = new EmbedBuilder();
-				meb.setColor(ColorUtils.getRainbowColor(2000));
+		if (message.length() > getPrefix().length()) {
+			q = "";
+			for (int i = getPrefix().length() + 1; i < message.length(); i++)
+				q += message.charAt(i);
+		}
 
-				meb.setTitle("Error: No Arguments provided!");
-				meb.setDescription("Please add an argument like " + this.getPrefix() + " `<args>`");
-				event.getChannel().sendMessage(meb.build()).queue();
-				return;
-			}
-
-			String url = "https://www.googleapis.com/youtube/v3/search/?" + "safeSearch=moderate" + "&regionCode=US"
-					+ "&q=" + URLEncoder.encode(q, "UTF-8") + "&type=channel&part=snippet&key="
-					+ Constants.GOOGLE_API_KEY;
-
-			JSONObject root = new JSONObject(Unirest.get(url).asString().getBody());
-
+		if (q == null) {
 			EmbedBuilder meb = new EmbedBuilder();
-
-			meb.setTitle("YouTube Subscribers");
 			meb.setColor(ColorUtils.getRainbowColor(2000));
 
-			if (!root.has("items")) {
-				meb.addField("No Results", "Unfortunately I couldn't find any results for `" + q + "`", true);
-				event.getChannel().sendMessage(meb.build()).queue();
-				return;
-			}
-
-			meb.addField("`Channel: `", "https://www.youtube.com/channel/"
-					+ root.getJSONArray("items").getJSONObject(0).getJSONObject("id").getString("channelId") + "\n",
-					false);
-
-			JSONObject subs = new JSONObject(Unirest
-					.get("https://www.googleapis.com/youtube/v3/channels?part=statistics&id="
-							+ root.getJSONArray("items").getJSONObject(0).getJSONObject("id").getString("channelId")
-							+ "&fields=items/statistics/subscriberCount&key=" + Constants.GOOGLE_API_KEY)
-					.asString().getBody());
-
-			meb.addField("`Subscribers: `", df.format(
-					subs.getJSONArray("items").getJSONObject(0).getJSONObject("statistics").getInt("subscriberCount"))
-					+ "\n", false);
-
+			meb.setTitle("Error: No Arguments provided!");
+			meb.setDescription("Please add an argument like " + this.getPrefix() + " `<args>`");
 			event.getChannel().sendMessage(meb.build()).queue();
-		} catch (Exception e) {
+			return;
 		}
+
+		String url = "https://invidious.snopyta.org/api/v1/search?type=channel&fields=authorUrl,subCount&q="
+				+ URLEncoder.encode(q, "UTF-8");
+
+		JSONArray jArray = Unirest.get(url).asJson().getBody().getArray();
+
+		EmbedBuilder meb = new EmbedBuilder();
+
+		meb.setTitle("YouTube Subscribers");
+		meb.setColor(ColorUtils.getRainbowColor(2000));
+
+		if (jArray.length() == 0) {
+			meb.addField("No Results", "Unfortunately I couldn't find any results for `" + q + "`", true);
+			event.getChannel().sendMessage(meb.build()).queue();
+			return;
+		}
+
+		JSONObject selected = jArray.getJSONObject(0);
+
+		meb.addField("`Channel: `", "https://www.youtube.com" + selected.getString("authorUrl") + "\n", false);
+
+		meb.addField("`Subscribers: `", df.format(selected.getInt("subCount")) + "\n", false);
+
+		event.getChannel().sendMessage(meb.build()).queue();
 	}
 }
