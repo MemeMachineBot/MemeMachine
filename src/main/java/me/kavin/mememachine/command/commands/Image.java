77f3184ffc01f8ab90744d5fa447b0c9658452dc
FireMasterk <20838718+FireMasterK@users.noreply.github.com>
2020-07-03 01:00:07 +0530

    Fix more issues.

--- src/main/java/me/kavin/mememachine/command/commands/Image.java
+++ src/main/java/me/kavin/mememachine/command/commands/Image.java
@@ -2,10 +2,9 @@ package me.kavin.mememachine.command.commands;
 
 import java.net.URLEncoder;
 
-import kong.unirest.json.JSONObject;
-
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import kong.unirest.Unirest;
+import kong.unirest.json.JSONObject;
 import me.kavin.mememachine.command.Command;
 import me.kavin.mememachine.consts.Constants;
 import me.kavin.mememachine.event.EventHandler;
@@ -13,7 +12,7 @@ import me.kavin.mememachine.event.events.EventGuildReactionAdd;
 import me.kavin.mememachine.utils.ColorUtils;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.guild.react.GenericGuildMessageReactionEvent;
 
@@ -27,114 +26,102 @@ public class Image extends Command {
 	}
 
 	@Override
-	public void onCommand(String message, MessageReceivedEvent event) {
-		try {
-			String q = null;
+	public void onCommand(String message, MessageReceivedEvent event) throws Exception {
+		String q = null;
 
-			if (message.length() > getPrefix().length()) {
-				q = "";
-				for (int i = getPrefix().length() + 1; i < message.length(); i++)
-					q += message.charAt(i);
-			}
+		if (message.length() > getPrefix().length()) {
+			q = "";
+			for (int i = getPrefix().length() + 1; i < message.length(); i++)
+				q += message.charAt(i);
+		}
 
-			Message sent = event.getChannel().sendMessage(getSearch(q)).submit().get();
+		EmbedBuilder meb = new EmbedBuilder();
+		String url = "https://www.googleapis.com/customsearch/v1?" + "safe=medium&searchType=image&" + "q="
+				+ URLEncoder.encode(q + " filetype:png", "UTF-8") + "&cx=008677437472124065250%3Ajljeb59kuse&num=1&key="
+				+ Constants.GOOGLE_API_KEY;
+		JSONObject root = new JSONObject(Unirest.get(url).asString().getBody());
+		meb.setTitle("Google Image Search: " + q);
+		meb.setColor(ColorUtils.getRainbowColor(2000));
+
+		System.out.println(root);
+
+		if (!root.has("items")) {
+			meb.addField("No Results", "Unfortunately I couldn't find any results for `" + q + "`", true);
+		} else {
+			meb.setImage(Constants.IMAGE_PROXY_URL
+					+ URLEncoder.encode(root.getJSONArray("items").getJSONObject(0).getString("link"), "UTF-8"));
+			meb.setDescription("Page 1 / 100");
+
+			Message sent = event.getChannel().sendMessage(meb.build()).submit().get();
 			sent.addReaction("◀").queue();
 			sent.addReaction("▶").queue();
 			this.queries.add(q);
 			this.sent.add(sent);
-		} catch (Exception e) {
 		}
-	}
-
-	private MessageEmbed getSearch(String q) {
-		try {
-			EmbedBuilder meb = new EmbedBuilder();
-			String url = "https://www.googleapis.com/customsearch/v1?" + "safe=medium&searchType=image&" + "q="
-					+ URLEncoder.encode(q + " filetype:png", "UTF-8")
-					+ "&cx=008677437472124065250%3Ajljeb59kuse&num=1&key=" + Constants.GOOGLE_API_KEY;
-			JSONObject root = new JSONObject(Unirest.get(url).asString().getBody());
-			meb.setTitle("Google Image Search: " + q);
-			meb.setColor(ColorUtils.getRainbowColor(2000));
-			if (!root.has("items")) {
-				meb.addField("No Results", "Unfortunately I couldn't find any results for `" + q + "`", true);
-				return meb.build();
-			}
-			meb.setImage(Constants.GOOGLE_PROXY_IMAGE
-					+ URLEncoder.encode(root.getJSONArray("items").getJSONObject(0).getString("link"), "UTF-8"));
-			meb.setDescription("Page 1 / 100");
-			return meb.build();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
 	}
 
 	@EventHandler
-	private void onReaction(EventGuildReactionAdd event) {
+	private void onReaction(EventGuildReactionAdd event) throws Exception {
 		GenericGuildMessageReactionEvent reactionEvent = event.getEvent();
 		for (int i = 0; i < sent.size(); i++) {
+
 			Message msg = sent.get(i);
-			if (msg.getIdLong() == reactionEvent.getMessageIdLong() && !reactionEvent.getUser().isBot()) {
+			User user = reactionEvent.getJDA().retrieveUserById(reactionEvent.getUserIdLong()).submit().get();
+
+			if (msg.getIdLong() == reactionEvent.getMessageIdLong() && !user.isBot()) {
 				switch (reactionEvent.getReactionEmote().getName()) {
-				case "◀":
-					try {
-						EmbedBuilder meb = new EmbedBuilder(msg.getEmbeds().get(0));
+				case "◀": {
+					EmbedBuilder meb = new EmbedBuilder(msg.getEmbeds().get(0));
 
-						int page = Integer.parseInt(meb.getDescriptionBuilder().toString().split(" ")[1]);
-						page--;
+					int page = Integer.parseInt(meb.getDescriptionBuilder().toString().split(" ")[1]);
+					page--;
 
-						if (page < 1)
-							return;
+					if (page < 1)
+						return;
 
-						int start = 10 * (page / 10);
-						String url = "https://www.googleapis.com/customsearch/v1?" + "safe=medium&searchType=image&"
-								+ "q=" + URLEncoder.encode(queries.get(i) + " filetype:png", "UTF-8")
-								+ "&cx=008677437472124065250%3Ajljeb59kuse&num=10" + "&key=" + Constants.GOOGLE_API_KEY;
-						if (start != 0)
-							url += "&start=" + start;
-						JSONObject root = new JSONObject(Unirest.get(url).asString().getBody());
+					int start = 10 * (page / 10);
+					String url = "https://www.googleapis.com/customsearch/v1?" + "safe=medium&searchType=image&" + "q="
+							+ URLEncoder.encode(queries.get(i) + " filetype:png", "UTF-8")
+							+ "&cx=008677437472124065250%3Ajljeb59kuse&num=10" + "&key=" + Constants.GOOGLE_API_KEY;
+					if (start != 0)
+						url += "&start=" + start;
+					JSONObject root = new JSONObject(Unirest.get(url).asString().getBody());
 
-						meb.setImage(Constants.GOOGLE_PROXY_IMAGE + URLEncoder.encode(root.getJSONArray("items")
-								.getJSONObject(page < 10 ? (page % 10) - 1 : page % 10).getString("link"), "UTF-8"));
-						meb.setDescription("Page " + page + " / 100");
+					meb.setImage(Constants.IMAGE_PROXY_URL + URLEncoder.encode(root.getJSONArray("items")
+							.getJSONObject(page < 10 ? (page % 10) - 1 : page % 10).getString("link"), "UTF-8"));
+					meb.setDescription("Page " + page + " / 100");
 
-						sent.set(i, msg.editMessage(meb.build()).submit().get());
+					sent.set(i, msg.editMessage(meb.build()).submit().get());
 
-						break;
-
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
 					break;
-				case "▶":
-					try {
-						EmbedBuilder meb = new EmbedBuilder(msg.getEmbeds().get(0));
+				}
 
-						int page = Integer.parseInt(meb.getDescriptionBuilder().toString().split(" ")[1]);
-						page++;
+				case "▶": {
+					EmbedBuilder meb = new EmbedBuilder(msg.getEmbeds().get(0));
 
-						if (page > 100)
-							return;
+					int page = Integer.parseInt(meb.getDescriptionBuilder().toString().split(" ")[1]);
+					page++;
 
-						int start = 10 * (page / 10);
-						String url = "https://www.googleapis.com/customsearch/v1?" + "safe=medium&searchType=image&"
-								+ "q=" + URLEncoder.encode(queries.get(i) + " filetype:png", "UTF-8")
-								+ "&cx=008677437472124065250%3Ajljeb59kuse&num=10" + "&key=" + Constants.GOOGLE_API_KEY;
-						if (start != 0)
-							url += "&start=" + start;
-						JSONObject root = new JSONObject(Unirest.get(url).asString().getBody());
+					if (page > 100)
+						return;
 
-						meb.setImage(Constants.GOOGLE_PROXY_IMAGE + URLEncoder.encode(root.getJSONArray("items")
-								.getJSONObject(page < 10 ? (page % 10) - 1 : page % 10).getString("link"), "UTF-8"));
-						meb.setDescription("Page " + page + " / 100");
+					int start = 10 * (page / 10);
+					String url = "https://www.googleapis.com/customsearch/v1?" + "safe=medium&searchType=image&" + "q="
+							+ URLEncoder.encode(queries.get(i) + " filetype:png", "UTF-8")
+							+ "&cx=008677437472124065250%3Ajljeb59kuse&num=10" + "&key=" + Constants.GOOGLE_API_KEY;
+					if (start != 0)
+						url += "&start=" + start;
+					JSONObject root = new JSONObject(Unirest.get(url).asString().getBody());
 
-						sent.set(i, msg.editMessage(meb.build()).submit().get());
+					meb.setImage(Constants.IMAGE_PROXY_URL + URLEncoder.encode(root.getJSONArray("items")
+							.getJSONObject(page < 10 ? (page % 10) - 1 : page % 10).getString("link"), "UTF-8"));
+					meb.setDescription("Page " + page + " / 100");
 
-						break;
+					sent.set(i, msg.editMessage(meb.build()).submit().get());
 
-					} catch (Exception e) {
-					}
 					break;
+				}
+
 				}
 				reactionEvent.getReaction().removeReaction(reactionEvent.getUser()).queue();
 			}
--- src/main/java/me/kavin/mememachine/command/commands/WYR.java
+++ src/main/java/me/kavin/mememachine/command/commands/WYR.java
@@ -1,5 +1,9 @@
 package me.kavin.mememachine.command.commands;
 
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+
 import org.jsoup.Jsoup;
 import org.jsoup.select.Elements;
 
@@ -10,7 +14,6 @@ import me.kavin.mememachine.event.EventHandler;
 import me.kavin.mememachine.event.events.EventGuildReactionAdd;
 import me.kavin.mememachine.event.events.EventGuildReactionRemove;
 import me.kavin.mememachine.utils.ColorUtils;
-import me.kavin.mememachine.utils.Multithreading;
 import me.kavin.mememachine.utils.WYRGame;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Message;
@@ -30,84 +33,77 @@ public class WYR extends Command {
 	}
 
 	@Override
-	public void onCommand(String message, MessageReceivedEvent event) {
-		try {
+	public void onCommand(String message, MessageReceivedEvent event) throws Exception {
 
-			EmbedBuilder meb = new EmbedBuilder();
+		EmbedBuilder meb = new EmbedBuilder();
 
-			meb.setTitle("Would You Rather");
-			meb.setColor(ColorUtils.getRainbowColor(2000));
+		meb.setTitle("Would You Rather");
+		meb.setColor(ColorUtils.getRainbowColor(2000));
 
-			Elements options = Jsoup.parse(Unirest.get("http://either.io/").asString().getBody())
-					.getElementsByClass("option-text");
+		Elements options = Jsoup.parse(Unirest.get("http://either.io/").asString().getBody())
+				.getElementsByClass("option-text");
 
-			String choice1 = options.get(0).text();
-			String choice2 = options.get(1).text();
+		String choice1 = options.get(0).text();
+		String choice2 = options.get(1).text();
 
-			meb.addField(emoji1, choice1 + "\n", false);
-			meb.addField(emoji2, choice2 + "\n", false);
+		meb.addField(emoji1, choice1 + "\n", false);
+		meb.addField(emoji2, choice2 + "\n", false);
 
-			Message msg = event.getChannel().sendMessage(meb.build()).submit().get();
+		Message msg = event.getChannel().sendMessage(meb.build()).submit().get();
 
-			msg.addReaction(emoji1).queue();
-			msg.addReaction(emoji2).queue();
+		msg.addReaction(emoji1).queue();
+		msg.addReaction(emoji2).queue();
 
-			sent.put(msg.getIdLong(), new WYRGame(choice1, choice2));
+		sent.put(msg.getIdLong(), new WYRGame(choice1, choice2));
 
-			Multithreading.runAsync(new Runnable() {
-				@Override
-				public void run() {
+		new Timer().schedule(new TimerTask() {
+			@Override
+			public void run() {
 
-					try {
-						Thread.sleep(45000);
-					} catch (Exception e) {
-					}
+				long msgId = msg.getIdLong();
+				WYRGame game = sent.get(msgId);
 
-					long msgId = msg.getIdLong();
-					WYRGame game = sent.get(msgId);
+				int votes1 = game.getVotes1();
+				int votes2 = game.getVotes2();
 
-					int votes1 = game.getVotes1();
-					int votes2 = game.getVotes2();
+				EmbedBuilder meb = new EmbedBuilder();
 
-					EmbedBuilder meb = new EmbedBuilder();
+				meb.setTitle("Would You Rather Results");
+				meb.setColor(ColorUtils.getRainbowColor(2000));
 
-					meb.setTitle("Would You Rather Results");
-					meb.setColor(ColorUtils.getRainbowColor(2000));
-
-					if (votes1 > votes2) {
-						meb.addField("", "The winner was `" + game.getChoice1() + "`", false);
-					} else if (votes2 > votes1) {
-						meb.addField("", "The winner was `" + game.getChoice2() + "`", false);
-					} else {
-						meb.addField("", "It was a `draw`", false);
-					}
-
-					event.getChannel().sendMessage(meb.build()).queue();
-
-					sent.remove(msgId);
+				if (votes1 > votes2) {
+					meb.addField("", "The winner was `" + game.getChoice1() + "`", false);
+				} else if (votes2 > votes1) {
+					meb.addField("", "The winner was `" + game.getChoice2() + "`", false);
+				} else {
+					meb.addField("", "It was a `draw`", false);
 				}
-			});
-		} catch (Exception e) {
-		}
+
+				event.getChannel().sendMessage(meb.build()).queue();
+
+				sent.remove(msgId);
+			}
+		}, TimeUnit.SECONDS.toMillis(45));
 	}
 
 	@EventHandler
 	private void onReactionAdd(EventGuildReactionAdd event) throws Exception {
 		GuildMessageReactionAddEvent reactionEvent = event.getEvent();
 
+		long msgId = reactionEvent.getMessageIdLong();
+
+		if (!sent.containsKey(msgId))
+			return;
+
 		User user = reactionEvent.getJDA().retrieveUserById(reactionEvent.getUserIdLong()).submit().get();
 
 		if (user.isBot())
 			return;
 
-		long msgId = reactionEvent.getMessageIdLong();
-
-		if (sent.containsKey(msgId)) {
-			if (reactionEvent.getReactionEmote().getName().equals(emoji1))
-				sent.get(msgId).addVotes1();
-			if (reactionEvent.getReactionEmote().getName().equals(emoji2))
-				sent.get(msgId).addVotes2();
-		}
+		if (reactionEvent.getReactionEmote().getName().equals(emoji1))
+			sent.get(msgId).addVotes1();
+		if (reactionEvent.getReactionEmote().getName().equals(emoji2))
+			sent.get(msgId).addVotes2();
 	}
 
 	@EventHandler
